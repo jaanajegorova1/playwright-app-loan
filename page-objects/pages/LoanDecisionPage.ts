059cb7341f92daf271e7c94149246ae6782c4d5b
jaana <jaana.jegorova@gmail.com>
2025-03-07 13:25:52 +0200

    TL-20/jj/lesson20_LoanApp

---
+++ page-objects/pages/LoanDecisionPage.ts
@@ -0,0 +1,25 @@
+import { Locator, Page } from "@playwright/test";
+
+export class LoanDecisionPage {
+  readonly page: Page;
+  readonly finalAmount: Locator;
+  readonly finalPeriod: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.finalAmount = page.getByTestId("final-page-amount");
+    this.finalPeriod = page.getByTestId("final-page-period");
+  }
+
+  async getFinalAmountValue(): Promise<string> {
+    const text = await this.finalAmount.innerText();
+
+    return text.split(" ")[0]; //tak kak hotim zabrat tolko 1 strochku, posle probela. to est zabrali 500
+  }
+
+  async getFinalPeriodValue(): Promise<string> {
+    const text = await this.finalPeriod.innerText();
+
+    return text.split(" ")[0];
+  }
+}
